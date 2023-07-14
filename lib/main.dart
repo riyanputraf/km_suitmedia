13b20f29815efaa50ee62fb9fed83470b3a1f5e9
riyanputraf <riyanputrafirjatullah@gmail.com>
2023-07-14 19:21:02 +0700

    add fontstyle Poppins

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Poppins'
+      ),
       home: FirstScreen()
     );
   }
